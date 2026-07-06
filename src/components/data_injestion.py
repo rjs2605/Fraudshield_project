1207621975f7af77efc484fde0229398edaca487
rjs2605 <127687656+rjs2605@users.noreply.github.com>
2026-07-06 10:40:57 +0530

    Update data_injestion.py

--- src/components/data_injestion.py
+++ src/components/data_injestion.py
@@ -1,11 +1,8 @@
 import pandas as pd
 
-
 class DataIngestion:
-
     def __init__(self,path="data/raw/creditcard.csv"):
         self.path = path
-
     def load_data(self):
         df = pd.read_csv(self.path)
         print("\nDataset Loaded")
