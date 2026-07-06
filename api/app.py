c864d8578641f5e01b15bd8c10bf9a2ddb5f8721
rjs2605 <127687656+rjs2605@users.noreply.github.com>
2026-07-06 10:40:15 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,16 +1,15 @@
 from fastapi import FastAPI
-from pydantic import BaseModel         #pydantic used for data validation
+from pydantic import BaseModel         
 import numpy as np
 import joblib
 
 app = FastAPI()
 
-#loads the model
 model = joblib.load("models/best_random_forest.pkl")
-scaler = joblib.load("models/scaler.pkl")             # loads the scaler from saved or dumped
+scaler = joblib.load("models/scaler.pkl")             
 
 class Transaction(BaseModel):
-    features: list[float]      # this accepts only list of float values
+    features: list[float]      
 
 @app.get("/")
 def home():
@@ -20,10 +19,7 @@ def home():
 
 @app.post("/predict")
 def predict(data: Transaction):
-
     arr = np.array(data.features).reshape(1, -1)
-
-    # scale the data form the inputs -- scale used reduce the range of the values in the columns
     arr = scaler.transform(arr)
     prediction = model.predict(arr)[0]
     probability = model.predict_proba(arr)[0][1]
