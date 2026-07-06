ac5ff7cf2cae59d3be4b1992ae54f212a5432054
rjs2605 <127687656+rjs2605@users.noreply.github.com>
2026-07-06 10:44:38 +0530

    Update model_tuner.py

--- src/components/model_tuner.py
+++ src/components/model_tuner.py
@@ -1,21 +1,17 @@
 from sklearn.model_selection import GridSearchCV
 
-
 class ModelTuner:
-
     def tune_random_forest(
         self,
         model,
         X_train,
         y_train
     ):
-
         params = {
             "n_estimators": [100, 200],
             "max_depth": [10, 20, None],
             "min_samples_split": [2, 5]      
         }
-
         grid = GridSearchCV(
             estimator=model,
             param_grid=params,
@@ -23,12 +19,9 @@ class ModelTuner:
             scoring="f1",
             n_jobs=-1
         )
-
         grid.fit(X_train, y_train)
-
         print("\n best Parameters")
         print(grid.best_params_)
-
         print("\n best F1")
         print(grid.best_score_)
 
